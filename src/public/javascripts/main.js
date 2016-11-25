95913a491ffd69ca7b6cee3eac2ac220f187b749
Óskar <Notandi@users.noreply.github.com>
2016-11-25 19:36:27 +0000

    Búinn að bæta við spinner sem er notaður meðan beðið er eftir api requests

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-  getMovies();
+  getUpcomingMovies();
   console.log("main.js fallið whats up in this biatch??!!");
   let tala = 550;
   getMovie({tala});
+
+  // breytir um myndir yfir í myndir sem eru væntanlegar þegar að ýtt er á "Væntanlegt" takkan
+  $('#playingNow').click(() => {
+    loading();
+    getMovies();
+  });
+
+    // breytir um myndir yfir í myndir sem eru í bíu þegar að ýtt er á "Í bíó" takkan
+  $('#playingSoon').click( () => {
+    loading();
+    getUpcomingMovies();
+  });
 });
 
 //THE MOVIE DATABASE functions
@@ -15,6 +27,15 @@ function getMovie(movieId) {
     displayMovie(resp);
   });
 
+}
+
+// fjarlægir allt úr content divinu og setur spinner í gang
+function loading(){
+  const sitecontent= $('#content');
+  sitecontent.empty();
+  const spinner = $('<div></div>');
+  spinner.attr('class','spinner');
+  sitecontent.append(spinner);
 }
 
 function displayMovie(response) {
@@ -30,7 +51,7 @@ function showMovies(movieData){
   sitecontent.empty();
   for (let i = 0; i < movieData.length ;i++ ){
     const movieDiv = $('<div></div>');
-    movieDiv.attr('class','col-xs-7 col-sm-5 col-md-4');
+    movieDiv.attr('class','col-xs-7 col-sm-5 col-md-4 col-lg-4');
     const moviePoster = $('<img>');
     moviePoster.attr('class','poster');
     moviePoster.attr('src',movieData[i].poster);
@@ -51,4 +72,16 @@ function getMovies(){
         showMovies(response);
       }
     });
+};
+
+function getUpcomingMovies() {
+  $.ajax({
+    url : 'http://localhost:3000/upcoming',
+    type : 'GET',
+    dataType : 'json',
+    success : function (response) {
+      console.log(response);
+      showMovies(response);
+    }
+  });
 };
